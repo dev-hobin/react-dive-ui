76238155c7186f9090b8d38ecd75b49e726fa22f
장호빈 <devhobin@gmail.com>
2023-08-11 19:54:36 +0900

    add: core workspace에 @xstate/react 패키지 추가

--- packages/core/src/Button.tsx
+++ packages/core/src/Button.tsx
@@ -1,11 +1,15 @@
 import { buttonMachine } from "@react-dive-ui/machines";
+import { useActor } from "@xstate/react";
 
 export interface ButtonProps {
   children: React.ReactNode;
 }
 
 export function Button(props: ButtonProps) {
-  console.log(buttonMachine);
+  const [state, send] = useActor(buttonMachine);
+
+  console.log(JSON.stringify(state.context, null, 2));
+
   return <button>{props.children}</button>;
 }
 
