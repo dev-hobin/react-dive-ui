6264cc4c98f4608e72332514fc152091d15b61de
장호빈 <devhobin@gmail.com>
2023-08-11 18:41:52 +0900

    feat: 네이밍 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-acme`
-  extends: ["acme"],
+  extends: ["dive"],
   settings: {
     next: {
       rootDir: ["apps/*/"],
