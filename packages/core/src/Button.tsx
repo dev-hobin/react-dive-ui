42b20295203c86f27e9a52340ac56a19cde996ff
장호빈 <devhobin@gmail.com>
2023-08-11 19:15:24 +0900

    feat: machines workspace 추가

--- packages/core/src/Button.tsx
+++ packages/core/src/Button.tsx
@@ -1,10 +1,11 @@
-import * as React from "react";
+import { buttonMachine } from "@react-dive-ui/machines";
 
 export interface ButtonProps {
   children: React.ReactNode;
 }
 
 export function Button(props: ButtonProps) {
+  console.log(buttonMachine);
   return <button>{props.children}</button>;
 }
 
