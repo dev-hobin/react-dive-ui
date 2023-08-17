3c2bf681fa1c700704cd6f3fc6211c39da45303d
장호빈 <devhobin@gmail.com>
2023-08-17 15:03:02 +0900

    feat: Accordion machine 로직 업데이트

--- packages/machines/accordion/src/machine.ts
+++ packages/machines/accordion/src/machine.ts
@@ -1,62 +1,119 @@
-import { assign, createMachine } from "xstate";
-import { Context, Events } from "./types";
+import { createMachine, assign } from "xstate";
+import { MachineContext, MachineEvents } from "./types";
 
 export const machine = createMachine(
   {
     id: "Accordion",
-    context: ({ input }) => ({
-      values: input?.values ?? [],
-      type: input?.type ?? "single",
-      collapsible: input?.collapsible ?? true,
-      disabled: input?.disabled ?? false,
-      orientation: input?.orientation ?? "vertical",
+    context: ({ input = {} }) => ({
+      type: input.type ?? "single",
+      itemMap: {},
+      orientation: input.orientation ?? "vertical",
+      collapsible: input.collapsible ?? true,
+      disabled: input.disabled ?? false,
+      value: input.value ?? [],
     }),
     initial: "idle",
     states: {
       idle: {
         on: {
-          "ITEM.TOGGLE": [
-            {
-              target: "idle",
-              guard: "isDisabled",
-            },
-            {
-              target: "idle",
-              actions: ["toggleItem"],
-            },
-          ],
+          "ITEM.REGISTER": {
+            target: "idle",
+            actions: ["registerItem"],
+          },
+          "ITEM.UNREGISTER": {
+            target: "idle",
+            actions: ["unregisterItem"],
+          },
+          "ITEM.TOGGLE": {
+            target: "idle",
+            actions: ["toggleItem"],
+          },
+          "ITEM.OPEN": {
+            target: "idle",
+            actions: ["openItem"],
+          },
+          "ITEM.CLOSE": {
+            target: "idle",
+            actions: ["closeItem"],
+          },
         },
       },
     },
     types: {
-      events: {} as Events,
-      context: {} as Context,
+      events: {} as MachineEvents,
+      context: {} as MachineContext,
     },
   },
   {
     actions: {
+      registerItem: assign({
+        itemMap: ({ context, event }) => {
+          if (event.type !== "ITEM.REGISTER") return context.itemMap;
+          const item = event.item;
+          context.itemMap[item.value] = item;
+          return { ...context.itemMap };
+        },
+      }),
+      unregisterItem: assign({
+        itemMap: ({ context, event }) => {
+          if (event.type !== "ITEM.UNREGISTER") return context.itemMap;
+          const itemKey = event.value;
+          delete context.itemMap[itemKey];
+          return { ...context.itemMap };
+        },
+      }),
       toggleItem: assign(({ context, event }) => {
-        if (event.type !== "ITEM.TOGGLE") return {};
-        const { type, values, collapsible } = context;
+        if (event.type !== "ITEM.TOGGLE") return context;
+        const { type, collapsible, value: currentOpened } = context;
         const { value } = event;
+
         if (type === "single") {
-          if (values[0] === value && collapsible) {
-            return { values: [] };
+          const currentOpenedValue = currentOpened[0];
+          // 이미 열려있는 아이템
+          if (currentOpenedValue === value) {
+            // collapse 가능하면 아이템 닫기
+            if (collapsible) return { value: [] };
+            // collapse 불가능하면 기존 상태 유지
+            else return { value: currentOpened };
           } else {
-            return { values: [value] };
+            // 열려있는 아이템 변경
+            return { value: [value] };
           }
-        } else if (type === "multiple") {
-          if (values.includes(value)) {
-            return { values: values.filter((v) => v === value) };
+        } else {
+          const currentOpenedValues = currentOpened;
+          if (currentOpenedValues.includes(value)) {
+            return { value: currentOpenedValues.filter((v) => v !== value) };
           } else {
-            return { values: [...values, value] };
+            return { value: [...currentOpenedValues, value] };
           }
         }
-        return {};
       }),
-    },
-    guards: {
-      isDisabled: ({ context }) => !!context.disabled,
+      openItem: assign(({ context, event }) => {
+        if (event.type !== "ITEM.OPEN") return context;
+        const { type, value: currentOpened } = context;
+        const { value } = event;
+
+        if (currentOpened.includes(value)) return context;
+        if (type === "single") {
+          return { value: [value] };
+        } else {
+          return { value: [...currentOpened, value] };
+        }
+      }),
+      closeItem: assign(({ context, event }) => {
+        if (event.type !== "ITEM.CLOSE") return context;
+        const { type, value: currentOpened, collapsible } = context;
+        const { value } = event;
+
+        if (!currentOpened.includes(value)) return context;
+
+        if (type === "single") {
+          if (collapsible) return { value: [] };
+          return { value: currentOpened };
+        } else {
+          return { value: currentOpened.filter((v) => v !== value) };
+        }
+      }),
     },
   }
 );
--- packages/machines/accordion/src/types.ts
+++ packages/machines/accordion/src/types.ts
@@ -1,9 +1,28 @@
-export type Context = {
-  values: string[];
-  type: "single" | "multiple";
+export type AccordionType = "single" | "multiple";
+export type AccordionOrientation = "vertical" | "horizontal";
+
+export interface AccordionItem {
+  value: string | number;
+  isDisabled: boolean;
+}
+
+export type MachineContext = {
+  type: AccordionType;
+  orientation: AccordionOrientation;
   collapsible: boolean;
   disabled: boolean;
-  orientation: "vertical" | "horizontal";
+  itemMap: Record<AccordionItem["value"], AccordionItem>;
+  value: AccordionItem["value"][];
 };
 
-export type Events = { type: "ITEM.TOGGLE"; value: string };
+export type MachineEvents =
+  | { type: "ITEM.REGISTER"; item: AccordionItem }
+  | { type: "ITEM.UNREGISTER"; value: AccordionItem["value"] }
+  | { type: "ITEM.TOGGLE"; value: AccordionItem["value"] }
+  | { type: "ITEM.OPEN"; value: AccordionItem["value"] }
+  | { type: "ITEM.CLOSE"; value: AccordionItem["value"] };
+// | {
+//     type: "ITEM.UPDATE";
+//     value: AccordionItem["value"];
+//     updated: Partial<AccordionItem>;
+//   };
