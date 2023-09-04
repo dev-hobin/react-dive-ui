0882887156ae275ac34b2f0432c8ac2735b849b7
장호빈 <devhobin@gmail.com>
2023-09-04 23:57:14 +0900

    feat: global style, theme 정의

---
+++ apps/docs/stories/accordion/style.css.ts
@@ -0,0 +1,66 @@
+import { vars } from "../../styles/theme.css";
+import { style } from "@vanilla-extract/css";
+
+export const root = style({
+  borderRadius: "0.25rem",
+  width: "20rem",
+  backgroundColor: vars.colors.gray[100],
+  boxShadow: `0 2px 10px ${vars.colors.alphas.black[50]}`,
+});
+
+export const item = style({
+  overflow: "hidden",
+  marginTop: "1px",
+
+  ":first-child": {
+    marginTop: 0,
+    borderTopLeftRadius: "0.25rem",
+    borderTopRightRadius: "0.25rem",
+  },
+
+  ":last-child": {
+    borderBottomLeftRadius: "0.25rem",
+    borderBottomRightRadius: "0.25rem",
+  },
+
+  ":focus-within": {
+    boxShadow: `0 0 0 1.5px ${vars.colors.black}`,
+    position: "relative",
+    zIndex: 1,
+  },
+});
+
+export const heading = style({
+  all: "unset",
+  display: "flex",
+});
+
+export const trigger = style({
+  all: "unset",
+
+  backgroundColor: vars.colors.white,
+  padding: "0 1.125rem",
+  height: "2.8125rem",
+  flex: 1,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  fontSize: vars.fontSizes.md,
+  fontWeight: vars.fontWeights.bold,
+  lineHeight: vars.lineHeights.none,
+  color: vars.colors.purple[800],
+});
+
+export const panel = style({
+  overflow: "hidden",
+  fontSize: vars.fontSizes.md,
+  color: vars.colors.alphas.black[800],
+  backgroundColor: vars.colors.gray[50],
+  padding: "1rem 1.25rem",
+
+  selectors: {
+    "&[data-state='closed']": {
+      display: "none",
+    },
+  },
+});
