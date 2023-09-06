acc7842cc7c318752fa9fc8dfac9462d77191feb
장호빈 <devhobin@gmail.com>
2023-09-06 21:30:55 +0900

    feat: RadioGroup 상태머신 로직 구현

--- packages/machines/radio-group/src/machine.ts
+++ packages/machines/radio-group/src/machine.ts
@@ -1,37 +1,116 @@
-import { createMachine } from "xstate";
+import { assign, createMachine, pure, raise } from "xstate";
+import { MachineContext, MachineEvent } from "./types";
+import { dom } from "./dom";
 
-export const machine = createMachine({
-  id: "RadioGroup",
-  initial: "idle",
-  states: {
-    idle: {
-      on: {
-        "RADIO.FOCUS": {
-          target: "focused",
+export const machine = createMachine(
+  {
+    id: "RadioGroup",
+    initial: "idle",
+    context: ({ input }) => ({
+      id: input?.id,
+      ids: input?.ids ?? null,
+      value: input?.value ?? null,
+      focusedValue: null,
+      disabled: input?.disabled ?? false,
+      orientation: input?.orientation ?? "vertical",
+      form: input?.form ?? null,
+    }),
+    states: {
+      idle: {
+        on: {
+          "RADIO.FOCUS": {
+            target: "focused",
+            actions: ["setFocusedValue"],
+          },
+        },
+      },
+      focused: {
+        on: {
+          "RADIO.BLUR": {
+            target: "idle",
+            actions: ["unsetFocusedValue"],
+          },
+          "RADIO.SELECT.NEXT": {
+            actions: ["focusNextRadio", "selectCurrentFocused"],
+          },
+          "RADIO.SELECT.PREV": {
+            actions: ["focusPrevRadio", "selectCurrentFocused"],
+          },
         },
       },
     },
-    focused: {
-      on: {
-        "RADIO.BLUR": {
-          target: "idle",
-        },
-        "RADIO.SELECT.NEXT": {},
-        "RADIO.SELECT.PREV": {},
+    on: {
+      "RADIO.SELECT": {
+        actions: ["setValue"],
+      },
+      "CONTEXT.SET": {
+        actions: ["setContext"],
       },
     },
+    types: {
+      context: {} as MachineContext,
+      events: {} as MachineEvent,
+    },
   },
-  on: {
-    "RADIO.SELECT": {},
-    "CONTEXT.SET": {},
-  },
-  types: {
-    events: {} as
-      | { type: "RADIO.FOCUS" }
-      | { type: "RADIO.BLUR" }
-      | { type: "RADIO.SELECT" }
-      | { type: "RADIO.SELECT.NEXT" }
-      | { type: "RADIO.SELECT.PREV" }
-      | { type: "CONTEXT.SET" },
-  },
-});
+  {
+    actions: {
+      setFocusedValue: pure(({ event }) => {
+        if (event.type !== "RADIO.FOCUS") return;
+        return assign({ focusedValue: event.value });
+      }),
+      unsetFocusedValue: assign({ focusedValue: null }),
+      focusNextRadio: ({ context }) => {
+        if (!context.focusedValue) return;
+
+        const currentRadioEl = dom.getRadioEl(context, context.focusedValue);
+        if (!currentRadioEl) return;
+
+        const radioEls = dom.getRadioEls(context);
+        if (radioEls.length === 0) return;
+
+        const currentIndex = radioEls.indexOf(currentRadioEl);
+        if (currentIndex === -1) return;
+
+        radioEls[getNextIndex(currentIndex, radioEls)]?.focus();
+      },
+      focusPrevRadio: ({ context }) => {
+        if (!context.focusedValue) return;
+
+        const currentRadioEl = dom.getRadioEl(context, context.focusedValue);
+        if (!currentRadioEl) return;
+
+        const radioEls = dom.getRadioEls(context);
+        if (radioEls.length === 0) return;
+
+        const currentIndex = radioEls.indexOf(currentRadioEl);
+        if (currentIndex === -1) return;
+
+        radioEls.at(getPrevIndex(currentIndex, radioEls))?.focus();
+      },
+      selectCurrentFocused: pure(({ context }) => {
+        if (!context.focusedValue) return;
+        return raise({
+          type: "RADIO.SELECT",
+          value: context.focusedValue,
+        });
+      }),
+      setValue: pure(({ event }) => {
+        if (event.type !== "RADIO.SELECT") return;
+        return assign({ value: event.value });
+      }),
+      setContext: pure(({ event }) => {
+        if (event.type !== "CONTEXT.SET") return;
+        return assign(event.context);
+      }),
+    },
+  }
+);
+
+function getNextIndex(currentIndex: number, array: unknown[]) {
+  if (array.length === 0) return 0;
+  return (currentIndex + 1) % array.length;
+}
+function getPrevIndex(currentIndex: number, array: unknown[]) {
+  if (array.length === 0) return 0;
+  return -((currentIndex + 1) % array.length);
+}
--- packages/machines/radio-group/src/types.ts
+++ packages/machines/radio-group/src/types.ts
@@ -4,14 +4,35 @@ import { machine } from "./machine";
 export type MachineState = StateFrom<typeof machine>;
 export type MachineSend = ActorRefFrom<typeof machine>["send"];
 export type MachineContext = {
-  // TODO
+  id: string;
+  ids: ElementIds | null;
+  value: string;
+  focusedValue: string | null;
+  disabled: boolean;
+  orientation: "vertical" | "horizontal";
+  form: FormOption | null;
 };
 
 export type ElementIds = Partial<{
-  // TODO
+  group: string;
+  radio(value: string): string;
+  label(value: string): string;
+  indicator(value: string): string;
 }>;
 
-export type MachineEvent = { type: string };
+export type FormOption = {
+  name: string;
+  required?: boolean;
+  readonly?: boolean;
+};
+
+export type MachineEvent =
+  | { type: "RADIO.FOCUS"; value: string }
+  | { type: "RADIO.BLUR" }
+  | { type: "RADIO.SELECT"; value: string }
+  | { type: "RADIO.SELECT.NEXT" }
+  | { type: "RADIO.SELECT.PREV" }
+  | { type: "CONTEXT.SET"; context: Partial<MachineContext> };
 
 export type ChangeDetails = {
   value: string | null;
