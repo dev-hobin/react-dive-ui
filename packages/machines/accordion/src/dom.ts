83c7493313aba777d425203733aa82b6f45bcbb3
장호빈 <devhobin@gmail.com>
2023-09-18 19:26:56 +0900

    feat: dom 아이디 가져오는 유틸 추가

--- packages/machines/accordion/src/dom.ts
+++ packages/machines/accordion/src/dom.ts
@@ -1,1 +1,11 @@
-export const dom = {};
+import { Context, Item } from "./types";
+
+export const dom = {
+  getRootId: (context: Context) => `accordion::root::${context.id}`,
+  getTriggerId: (context: Context, value: Item["value"]) =>
+    `accordion::trigger::${context.id}::${value}`,
+  getHeadingId: (context: Context, value: Item["value"]) =>
+    `accordion::heading::${context.id}::${value}`,
+  getPanelId: (context: Context, value: Item["value"]) =>
+    `accordion::panel::${context.id}::${value}`,
+};
