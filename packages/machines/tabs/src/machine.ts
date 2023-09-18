5b169e5f3d60eaf483b4011b996a02a3b04e86e9
장호빈 <devhobin@gmail.com>
2023-09-18 22:43:12 +0900

    feat: 기본 machine 로직 작성

--- packages/machines/tabs/src/machine.ts
+++ packages/machines/tabs/src/machine.ts
@@ -1,24 +1,117 @@
-import { createMachine } from "xstate";
-import { Context, Input } from "./types";
+import { createMachine, pure, raise } from "xstate";
+import { Actions, Context, Events, Guards, Input } from "./types";
 
-export const machine = createMachine({
-  id: "Tabs",
-  context: ({ input }) => ({
-    id: input.id,
-    focusedValue: null,
-    value: input?.value ?? null,
-    itemMap: input?.itemMap ?? new Map(),
-    orientation: input?.orientation ?? "horizontal",
-    activationMode: input?.activationMode ?? "automatic",
-  }),
-  initial: "idle",
-  states: {
-    idle: {},
-    focused: {},
+export const machine = createMachine(
+  {
+    id: "Tabs",
+    context: ({ input }) => ({
+      id: input.id,
+      focusedValue: null,
+      value: input?.value ?? null,
+      itemMap: input?.itemMap ?? new Map(),
+      orientation: input?.orientation ?? "horizontal",
+      activationMode: input?.activationMode ?? "automatic",
+    }),
+    initial: "idle",
+    states: {
+      idle: {
+        on: {
+          "TRIGGER.FOCUSED": {
+            target: "focused",
+            actions: [
+              {
+                type: "setFocusedValue",
+                params: ({ event }) => ({ value: event.value }),
+              },
+            ],
+          },
+        },
+      },
+      focused: {
+        on: {
+          "TRIGGER.BLURRED": {
+            target: "idle",
+            actions: [
+              {
+                type: "setFocusedValue",
+                params: { value: null },
+              },
+            ],
+          },
+          "TRIGGER.FOCUS.NEXT": [
+            {
+              guard: "isAutomaticMode",
+              actions: [
+                "focusNextTrigger",
+                pure(({ context }) => {
+                  if (!context.focusedValue) return;
+                  return raise({
+                    type: "ITEM.ACTIVATE",
+                    value: context.focusedValue,
+                  });
+                }),
+              ],
+            },
+            {
+              actions: ["focusNextTrigger"],
+            },
+          ],
+          "TRIGGER.FOCUS.PREV": [
+            {
+              guard: "isAutomaticMode",
+              actions: [
+                "focusPrevTrigger",
+                pure(({ context }) => {
+                  if (!context.focusedValue) return;
+                  return raise({
+                    type: "ITEM.ACTIVATE",
+                    value: context.focusedValue,
+                  });
+                }),
+              ],
+            },
+            {
+              actions: ["focusPrevTrigger"],
+            },
+          ],
+        },
+      },
+    },
+    on: {
+      "ITEM.ACTIVATE": [
+        {
+          guard: {
+            type: "isItemDisabled",
+            params: ({ event }) => ({ value: event.value }),
+          },
+        },
+        {
+          actions: [
+            {
+              type: "setValue",
+              params: ({ event }) => ({ value: event.value }),
+            },
+          ],
+        },
+      ],
+    },
+    types: {
+      context: {} as Context,
+      input: {} as Input,
+      events: {} as Events,
+      actions: {} as Actions,
+      guards: {} as Guards,
+    },
   },
-  on: {},
-  types: {
-    context: {} as Context,
-    input: {} as Input,
-  },
-});
+  {
+    guards: {
+      isItemDisabled: ({ context, guard }) => {
+        const item = context.itemMap.get(guard.params.value);
+        return item?.disabled ?? true;
+      },
+      isAutomaticMode: ({ context }) => {
+        return context.activationMode === "automatic";
+      },
+    },
+  }
+);
--- packages/machines/tabs/src/types.ts
+++ packages/machines/tabs/src/types.ts
@@ -16,6 +16,29 @@ export type Context = {
   orientation: Orientation;
 };
 
+export type Events =
+  | { type: "TRIGGER.FOCUSED"; value: Item["value"] }
+  | { type: "TRIGGER.BLURRED" }
+  | { type: "TRIGGER.FOCUS.NEXT" }
+  | { type: "TRIGGER.FOCUS.PREV" }
+  | { type: "ITEM.ACTIVATE"; value: Item["value"] };
+
+export type Actions =
+  | {
+      type: "setFocusedValue";
+      params: { value: Item["value"] | null };
+    }
+  | { type: "focusNextTrigger" }
+  | { type: "focusPrevTrigger" }
+  | { type: "setValue"; params: { value: Item["value"] } };
+
+export type Guards =
+  | {
+      type: "isItemDisabled";
+      params: { value: Item["value"] };
+    }
+  | { type: "isAutomaticMode" };
+
 export type Input = Pick<Context, "id"> &
   Partial<
     Pick<Context, "value" | "itemMap" | "activationMode" | "orientation">
