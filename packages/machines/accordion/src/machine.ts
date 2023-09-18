3e1711d67ed6d7b518cd2126fbe118cb265bb667
장호빈 <devhobin@gmail.com>
2023-09-18 13:33:27 +0900

    feat: accordion machine 컨텍스트, 인풋 정의

--- packages/machines/accordion/src/machine.ts
+++ packages/machines/accordion/src/machine.ts
@@ -1,12 +1,44 @@
 import { createMachine } from "xstate";
 
+type Item = {
+  value: string;
+  disabled: boolean;
+};
+
+type Context = {
+  id: string;
+  type: "single" | "multiple";
+  focusedValue: Item["value"] | null;
+  expandedValues: Item["value"][];
+  itemMap: Map<Item["value"], Item> | null;
+  collapsible: boolean;
+  orientation: "vertical" | "horizontal";
+};
+
+type Input = Pick<Context, "id" | "type"> &
+  Partial<
+    Pick<Context, "expandedValues" | "itemMap" | "collapsible" | "orientation">
+  >;
+
 export const machine = createMachine({
   id: "Accordion",
   initial: "idle",
-  context: ({ input }) => ({}),
+  context: ({ input }) => ({
+    id: input.id,
+    type: input.type,
+    focusedValue: null,
+    expandedValues: input.expandedValues ?? [],
+    itemMap: input.itemMap ?? new Map(),
+    collapsible: input.collapsible ?? false,
+    orientation: input.orientation ?? "vertical",
+  }),
   states: {
     idle: {},
     focused: {},
   },
   on: {},
+  types: {
+    context: {} as Context,
+    input: {} as Input,
+  },
 });
