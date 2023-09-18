9d53ab2ee259673fa3f359fddb9599f5febd30af
장호빈 <devhobin@gmail.com>
2023-09-19 01:47:47 +0900

    remove: 필요없는 util 제거

--- packages/utils/useLatestValue/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { useLatestValue } from "./useLatestValue";
--- packages/utils/useLatestValue/src/useLatestValue.ts
+++
@@ -1,7 +0,0 @@
-import { useMemo, useRef } from "react";
-
-export const useLatestValue = <T>(value: T) => {
-  const ref = useRef<T>(value);
-  ref.current = value;
-  return useMemo(() => ref.current, []);
-};
