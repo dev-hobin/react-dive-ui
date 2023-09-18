3b7e8660a793496c7e1efa4d4acc9a0440da7f03
장호빈 <devhobin@gmail.com>
2023-09-18 22:49:00 +0900

    feat: dom 로직 작성

--- packages/machines/tabs/src/dom.ts
+++ packages/machines/tabs/src/dom.ts
@@ -1,1 +1,24 @@
-export const dom = {};
+import { Context, Item } from "./types";
+
+export const dom = {
+  getRootId: (context: Context) => `tabs::root::${context.id}`,
+  getTriggerId: (context: Context, value: Item["value"]) =>
+    `tabs::trigger::${context.id}::${value}`,
+  getPanelId: (context: Context, value: Item["value"]) =>
+    `tabs::panel::${context.id}::${value}`,
+  getListId: (context: Context, value: Item["value"]) =>
+    `tabs::list::${context.id}::${value}`,
+
+  getTriggerEls: (context: Context) => {
+    const triggerValues = Array.from(context.itemMap.keys());
+
+    const selector = triggerValues
+      .map(
+        (value) =>
+          `#${CSS.escape(dom.getTriggerId(context, value))}:not([disabled])`
+      )
+      .join(", ");
+
+    return Array.from<HTMLElement>(document.querySelectorAll(selector));
+  },
+};
