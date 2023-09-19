50b50df43d47c05e083b99b34d7a168e69187e5e
장호빈 <devhobin@gmail.com>
2023-09-19 15:02:04 +0900

    feat: 상태머신 기본 세팅

---
+++ packages/machines/checkbox/src/machine.ts
@@ -0,0 +1,86 @@
+import { assign, createMachine } from "xstate";
+import { Context, Events, Actions, Guards, CheckedState } from "./types";
+
+export const machine = createMachine(
+  {
+    id: "Checkbox",
+    context: {
+      checkedState: "checked",
+      disabled: false,
+    },
+    initial: "idle",
+    states: {
+      idle: {
+        on: {
+          CHECK: [
+            { guard: "isDisabled" },
+            {
+              guard: "isIndeterminate",
+              actions: [
+                {
+                  type: "setCheckedState",
+                  params: {
+                    checkedState: "checked",
+                  },
+                },
+              ],
+            },
+            { actions: ["toggleCheckedState"] },
+          ],
+          "SET.INDETERMINATE": {
+            actions: [
+              {
+                type: "setCheckedState",
+                params: { checkedState: "indeterminate" },
+              },
+            ],
+          },
+          "SET.DISABLED": {
+            actions: [
+              {
+                type: "setDisabled",
+                params: ({ event }) => ({ disabled: event.disabled }),
+              },
+            ],
+          },
+          "SET.CHECKED": {
+            actions: [
+              {
+                type: "setCheckedState",
+                params: ({ event }) => ({
+                  checkedState: event.checked ? "checked" : "unchecked",
+                }),
+              },
+            ],
+          },
+        },
+      },
+    },
+    types: {
+      context: {} as Context,
+      events: {} as Events,
+      actions: {} as Actions,
+      guards: {} as Guards,
+    },
+  },
+  {
+    actions: {
+      setCheckedState: assign(({ action }) => ({
+        checkedState: action.params.checkedState,
+      })),
+      toggleCheckedState: assign(({ context }) => {
+        const checkedState: CheckedState =
+          context.checkedState === "checked" ? "unchecked" : "checked";
+        return { checkedState };
+      }),
+      setDisabled: assign(({ action }) => ({
+        disabled: action.params.disabled,
+      })),
+    },
+    guards: {
+      isDisabled: ({ context }) => context.disabled,
+      isIndeterminate: ({ context }) =>
+        context.checkedState === "indeterminate",
+    },
+  }
+);
---
+++ packages/machines/checkbox/src/types.ts
@@ -0,0 +1,27 @@
+import { ActorRefFrom } from "xstate";
+import { machine } from "./machine";
+
+export type CheckedState = "checked" | "unchecked" | "indeterminate";
+
+export type Context = {
+  checkedState: CheckedState;
+  disabled: boolean;
+};
+
+export type Events =
+  | { type: "CHECK" }
+  | { type: "SET.CHECKED"; checked: boolean }
+  | { type: "SET.DISABLED"; disabled: boolean }
+  | { type: "SET.INDETERMINATE" };
+
+export type Actions =
+  | { type: "toggleCheckedState" }
+  | {
+      type: "setCheckedState";
+      params: { checkedState: CheckedState };
+    }
+  | { type: "setDisabled"; params: { disabled: boolean } };
+
+export type Guards = { type: "isDisabled" } | { type: "isIndeterminate" };
+
+export type Service = ActorRefFrom<typeof machine>;
