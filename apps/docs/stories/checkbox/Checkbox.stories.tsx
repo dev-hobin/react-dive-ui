487db176a04afd45c0f7cd8ccbeb4281de30b1da
장호빈 <devhobin@gmail.com>
2023-09-19 22:12:49 +0900

    feat: 체크박스 기본 스타일 작업

--- apps/docs/stories/checkbox/Checkbox.stories.tsx
+++ apps/docs/stories/checkbox/Checkbox.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta } from "@storybook/react";
 import { Checkbox, useCheckbox } from "@react-dive-ui/checkbox";
+import { controlStyle, iconStyle, labelStyle } from "./style.css";
+import { BsCheck } from "react-icons/bs";
 
 const meta = {
   title: "Component/Checkbox",
@@ -11,26 +13,17 @@ const meta = {
 export default meta;
 
 export const Default = () => {
-  const { apis, service } = useCheckbox();
+  const { service } = useCheckbox();
+
   return (
-    <div>
-      <form
-        onSubmit={(ev) => {
-          ev.preventDefault();
-          console.log(ev);
-        }}
-        onChange={(ev) => console.log(ev)}
-      >
-        <button type="button" onClick={() => apis.setIndeterminate()}>
-          set indeterminate
-        </button>
-        <Checkbox.Provider service={service}>
-          <Checkbox.Control>Control</Checkbox.Control>
-          <Checkbox.Label>라벨</Checkbox.Label>
-          <Checkbox.HiddenInput />
-        </Checkbox.Provider>
-        <button type="submit">submit</button>
-      </form>
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <Checkbox.Provider service={service}>
+        <Checkbox.Control className={controlStyle}>
+          <BsCheck className={iconStyle} />
+        </Checkbox.Control>
+        <Checkbox.Label className={labelStyle}>라벨 텍스트</Checkbox.Label>
+        <Checkbox.HiddenInput />
+      </Checkbox.Provider>
     </div>
   );
 };
