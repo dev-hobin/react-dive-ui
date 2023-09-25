29f2e10e3a33af5af51a1586ebcd0079a270f65e
장호빈 <devhobin@gmail.com>
2023-09-25 14:39:58 +0900

    feat: 다이얼로그 컴포넌트 구조

--- apps/docs/stories/dialog/Dialog.stories.tsx
+++ apps/docs/stories/dialog/Dialog.stories.tsx
@@ -8,5 +8,22 @@ const meta = {
 export default meta;
 
 export const Default = () => {
-  return <div>Dialog</div>;
+  return (
+    <div>
+      <button type="button" data-part="trigger">
+        Trigger
+      </button>
+
+      <div data-part="portal">
+        <div data-part="overlay"></div>
+        <div data-part="panel">
+          <h2 data-part="title">Title</h2>
+          <p data-part="description">description</p>
+          <button type="button" data-part="close">
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
