1b3644864607d8456f0e952a64e26b2853b38e72
장호빈 <devhobin@gmail.com>
2023-10-03 23:38:55 +0900

    feat: dismiss-logic 따로 패키지로 분리

--- packages/machines/dialog/src/machine.ts
+++ packages/machines/dialog/src/machine.ts
@@ -1,46 +1,12 @@
-import { dismissHandler } from "@react-dive-ui/dismissible-layer";
+import {
+  dismissLogic,
+  DismissLogicOptions,
+} from "@react-dive-ui/dismiss-logic";
 import { assign, createMachine, fromCallback, raise } from "xstate";
 import { createFocusTrap } from "focus-trap";
 
 import { Context, Events, Input } from "./types";
 import { dom } from "./dom";
-
-type DismissLogicOptions = {
-  getElement: () => HTMLElement | null;
-  dismiss: () => void;
-  modal: boolean;
-  exclude?: Array<(() => HTMLElement | null) | HTMLElement>;
-};
-const dismissLogic = fromCallback<any, DismissLogicOptions>(({ input }) => {
-  const cleanups: Array<() => void> = [];
-
-  const rId = requestAnimationFrame(() => {
-    const element = input.getElement();
-    if (!element) return;
-
-    const excludeElements = (input.exclude ?? [])
-      .map((v) => (typeof v === "function" ? v() : v))
-      .filter((v): v is HTMLElement => !!v);
-
-    cleanups.push(() => cancelAnimationFrame(rId));
-    cleanups.push(
-      dismissHandler({
-        layer: {
-          element,
-          dismiss: input.dismiss,
-          modal: input.modal,
-        },
-        options: {
-          exclude: excludeElements,
-        },
-      })
-    );
-  });
-
-  return () => {
-    cleanups.forEach((cleanup) => cleanup());
-  };
-});
 
 type ScrollLockOptions = {
   enabled: boolean;
