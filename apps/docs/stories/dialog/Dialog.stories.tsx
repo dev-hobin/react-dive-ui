9bbf36a0fac397e74246b3a463880654fa28679d
장호빈 <devhobin@gmail.com>
2023-10-03 20:10:23 +0900

    feat: 다이얼로그 기본적인 스타일링

--- apps/docs/stories/dialog/Dialog.stories.tsx
+++ apps/docs/stories/dialog/Dialog.stories.tsx
@@ -11,199 +11,35 @@ const meta = {
 
 export default meta;
 
-const Child = () => {
-  const { state: childState, service: childService } = useDialog();
-
-  const {
-    triggerProps: childTriggerProps,
-    closeProps: childCloseProps,
-    panelProps: childPanelProps,
-  } = connect(childService);
-
-  return (
-    <>
-      <button {...childTriggerProps} className={css.trigger}>
-        Child Trigger
-      </button>
-      {childState.open &&
-        createPortal(
-          <>
-            {/* <div className={css.backdrop}></div> */}
-            <div
-              {...childPanelProps}
-              className={css.panel}
-              style={{ width: 100 }}
-            >
-              <h2 data-part="title" className={css.title}>
-                Title
-              </h2>
-              <p data-part="description" className={css.description}>
-                description
-              </p>
-
-              <p>Child Content</p>
-
-              <button {...childCloseProps} className={css.close}>
-                Child Close
-              </button>
-            </div>
-          </>,
-          document.body
-        )}
-    </>
-  );
-};
-
 export const Default = () => {
   const { state, service } = useDialog();
 
   const { triggerProps, closeProps, panelProps, titleProps, descriptionProps } =
     connect(service);
+
   return (
     <div>
       <button {...triggerProps} className={css.trigger}>
-        Trigger
+        Open Dialog
       </button>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, omnis
-        blanditiis! Animi ea fugiat fuga rem, laudantium accusantium dignissimos
-        laborum assumenda sed repellendus asperiores quos? Molestiae sequi
-        assumenda animi sed quo hic nemo tempora qui maiores quam doloribus
-        earum deleniti veritatis corrupti harum saepe error nisi numquam
-        voluptatibus, neque accusamus quod placeat sint non. Voluptate, ipsum.
-        Temporibus nam at suscipit ipsum eius, alias rerum perspiciatis, debitis
-        a aliquam molestias corrupti, placeat error cum. Reiciendis dignissimos
-        maxime quaerat commodi corporis nulla molestias vel dolorem, sunt vitae
-        adipisci, repellendus nostrum possimus officia rerum dolorum, fuga totam
-        ratione esse nam illum alias accusantium?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, omnis
-        blanditiis! Animi ea fugiat fuga rem, laudantium accusantium dignissimos
-        laborum assumenda sed repellendus asperiores quos? Molestiae sequi
-        assumenda animi sed quo hic nemo tempora qui maiores quam doloribus
-        earum deleniti veritatis corrupti harum saepe error nisi numquam
-        voluptatibus, neque accusamus quod placeat sint non. Voluptate, ipsum.
-        Temporibus nam at suscipit ipsum eius, alias rerum perspiciatis, debitis
-        a aliquam molestias corrupti, placeat error cum. Reiciendis dignissimos
-        maxime quaerat commodi corporis nulla molestias vel dolorem, sunt vitae
-        adipisci, repellendus nostrum possimus officia rerum dolorum, fuga totam
-        ratione esse nam illum alias accusantium?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, omnis
-        blanditiis! Animi ea fugiat fuga rem, laudantium accusantium dignissimos
-        laborum assumenda sed repellendus asperiores quos? Molestiae sequi
-        assumenda animi sed quo hic nemo tempora qui maiores quam doloribus
-        earum deleniti veritatis corrupti harum saepe error nisi numquam
-        voluptatibus, neque accusamus quod placeat sint non. Voluptate, ipsum.
-        Temporibus nam at suscipit ipsum eius, alias rerum perspiciatis, debitis
-        a aliquam molestias corrupti, placeat error cum. Reiciendis dignissimos
-        maxime quaerat commodi corporis nulla molestias vel dolorem, sunt vitae
-        adipisci, repellendus nostrum possimus officia rerum dolorum, fuga totam
-        ratione esse nam illum alias accusantium?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, omnis
-        blanditiis! Animi ea fugiat fuga rem, laudantium accusantium dignissimos
-        laborum assumenda sed repellendus asperiores quos? Molestiae sequi
-        assumenda animi sed quo hic nemo tempora qui maiores quam doloribus
-        earum deleniti veritatis corrupti harum saepe error nisi numquam
-        voluptatibus, neque accusamus quod placeat sint non. Voluptate, ipsum.
-        Temporibus nam at suscipit ipsum eius, alias rerum perspiciatis, debitis
-        a aliquam molestias corrupti, placeat error cum. Reiciendis dignissimos
-        maxime quaerat commodi corporis nulla molestias vel dolorem, sunt vitae
-        adipisci, repellendus nostrum possimus officia rerum dolorum, fuga totam
-        ratione esse nam illum alias accusantium?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, omnis
-        blanditiis! Animi ea fugiat fuga rem, laudantium accusantium dignissimos
-        laborum assumenda sed repellendus asperiores quos? Molestiae sequi
-        assumenda animi sed quo hic nemo tempora qui maiores quam doloribus
-        earum deleniti veritatis corrupti harum saepe error nisi numquam
-        voluptatibus, neque accusamus quod placeat sint non. Voluptate, ipsum.
-        Temporibus nam at suscipit ipsum eius, alias rerum perspiciatis, debitis
-        a aliquam molestias corrupti, placeat error cum. Reiciendis dignissimos
-        maxime quaerat commodi corporis nulla molestias vel dolorem, sunt vitae
-        adipisci, repellendus nostrum possimus officia rerum dolorum, fuga totam
-        ratione esse nam illum alias accusantium?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, omnis
-        blanditiis! Animi ea fugiat fuga rem, laudantium accusantium dignissimos
-        laborum assumenda sed repellendus asperiores quos? Molestiae sequi
-        assumenda animi sed quo hic nemo tempora qui maiores quam doloribus
-        earum deleniti veritatis corrupti harum saepe error nisi numquam
-        voluptatibus, neque accusamus quod placeat sint non. Voluptate, ipsum.
-        Temporibus nam at suscipit ipsum eius, alias rerum perspiciatis, debitis
-        a aliquam molestias corrupti, placeat error cum. Reiciendis dignissimos
-        maxime quaerat commodi corporis nulla molestias vel dolorem, sunt vitae
-        adipisci, repellendus nostrum possimus officia rerum dolorum, fuga totam
-        ratione esse nam illum alias accusantium?
-      </p>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, omnis
-        blanditiis! Animi ea fugiat fuga rem, laudantium accusantium dignissimos
-        laborum assumenda sed repellendus asperiores quos? Molestiae sequi
-        assumenda animi sed quo hic nemo tempora qui maiores quam doloribus
-        earum deleniti veritatis corrupti harum saepe error nisi numquam
-        voluptatibus, neque accusamus quod placeat sint non. Voluptate, ipsum.
-        Temporibus nam at suscipit ipsum eius, alias rerum perspiciatis, debitis
-        a aliquam molestias corrupti, placeat error cum. Reiciendis dignissimos
-        maxime quaerat commodi corporis nulla molestias vel dolorem, sunt vitae
-        adipisci, repellendus nostrum possimus officia rerum dolorum, fuga totam
-        ratione esse nam illum alias accusantium?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, omnis
-        blanditiis! Animi ea fugiat fuga rem, laudantium accusantium dignissimos
-        laborum assumenda sed repellendus asperiores quos? Molestiae sequi
-        assumenda animi sed quo hic nemo tempora qui maiores quam doloribus
-        earum deleniti veritatis corrupti harum saepe error nisi numquam
-        voluptatibus, neque accusamus quod placeat sint non. Voluptate, ipsum.
-        Temporibus nam at suscipit ipsum eius, alias rerum perspiciatis, debitis
-        a aliquam molestias corrupti, placeat error cum. Reiciendis dignissimos
-        maxime quaerat commodi corporis nulla molestias vel dolorem, sunt vitae
-        adipisci, repellendus nostrum possimus officia rerum dolorum, fuga totam
-        ratione esse nam illum alias accusantium?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, omnis
-        blanditiis! Animi ea fugiat fuga rem, laudantium accusantium dignissimos
-        laborum assumenda sed repellendus asperiores quos? Molestiae sequi
-        assumenda animi sed quo hic nemo tempora qui maiores quam doloribus
-        earum deleniti veritatis corrupti harum saepe error nisi numquam
-        voluptatibus, neque accusamus quod placeat sint non. Voluptate, ipsum.
-        Temporibus nam at suscipit ipsum eius, alias rerum perspiciatis, debitis
-        a aliquam molestias corrupti, placeat error cum. Reiciendis dignissimos
-        maxime quaerat commodi corporis nulla molestias vel dolorem, sunt vitae
-        adipisci, repellendus nostrum possimus officia rerum dolorum, fuga totam
-        ratione esse nam illum alias accusantium?
-      </p>
 
       {state.open &&
         createPortal(
           <>
             <div className={css.backdrop}></div>
-            <div {...panelProps} className={css.panel}>
-              <h2 {...titleProps} data-part="title" className={css.title}>
-                Title
+            <article {...panelProps} className={css.panel}>
+              <h2 {...titleProps} className={css.title}>
+                Dialog Title
               </h2>
-              <p
-                {...descriptionProps}
-                data-part="description"
-                className={css.description}
-              >
-                description
+              <p {...descriptionProps} className={css.description}>
+                Dialog Description
               </p>
-              <Child />
-              <button {...closeProps} className={css.close}>
-                Close
-              </button>
-            </div>
+              <footer className={css.footer}>
+                <button {...closeProps} className={css.close}>
+                  OK
+                </button>
+              </footer>
+            </article>
           </>,
           document.body
         )}
--- apps/docs/stories/dialog/style.css.ts
+++ apps/docs/stories/dialog/style.css.ts
@@ -1,7 +1,25 @@
 import { style } from "@vanilla-extract/css";
 import { vars } from "../../styles/theme.css";
 
-export const trigger = style({});
+export const trigger = style({
+  color: vars.colors.black[500],
+  borderRadius: 8,
+  boxShadow: `0px 0px 10px 0px ${vars.colors.alphas.black[50]}`,
+  backgroundColor: vars.colors.white,
+  fontSize: vars.fontSizes.lg,
+  border: `1px solid transparent`,
+  padding: "0.4em 0.8em",
+  fontWeight: vars.fontWeights.medium,
+
+  ":hover": {
+    backgroundColor: vars.colors.gray[50],
+  },
+
+  ":focus": {
+    outline: `2px solid ${vars.colors.gray[200]}`,
+    outlineOffset: 2,
+  },
+});
 
 export const backdrop = style({
   position: "fixed",
@@ -11,13 +29,45 @@ export const backdrop = style({
 
 export const panel = style({
   position: "fixed",
-  inset: "calc(50% - 10rem)",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: "20rem",
   backgroundColor: vars.colors.white,
+  borderRadius: "1rem",
+  padding: "1rem",
 });
 
-export const close = style({});
+export const close = style({
+  color: vars.colors.black[500],
+  borderRadius: 8,
+  backgroundColor: vars.colors.white,
+  fontSize: vars.fontSizes.md,
+  border: `1px solid ${vars.colors.gray[100]}`,
+  padding: "0.4em 0.8em",
+  fontWeight: vars.fontWeights.medium,
 
-export const title = style({});
+  ":hover": {
+    backgroundColor: vars.colors.gray[50],
+  },
 
-export const description = style({});
+  ":focus": {
+    outline: `2px solid ${vars.colors.gray[200]}`,
+    outlineOffset: 2,
+  },
+});
+
+export const title = style({
+  fontWeight: vars.fontWeights.bold,
+  fontSize: vars.fontSizes["2xl"],
+});
+
+export const description = style({
+  fontWeight: vars.fontWeights.medium,
+  fontSize: vars.fontSizes.md,
+  color: vars.colors.gray[700],
+});
+
+export const footer = style({
+  marginTop: "1rem",
+});
