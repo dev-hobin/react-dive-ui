b75730d18b9ab39c3290cb9fc402ca3de01190a6
장호빈 <devhobin@gmail.com>
2023-10-10 01:21:25 +0900

    code update

--- packages/machines/tabs/src/index.ts
+++ packages/machines/tabs/src/index.ts
@@ -1,9 +1,3 @@
-export { machine } from "./machine";
-export { connect } from "./connect";
-export type {
-  Item,
-  Service,
-  Orientation,
-  ActivationMode,
-  ItemProp,
-} from "./types";
+export * from "./machine";
+export * from "./connect";
+export * from "./types";
